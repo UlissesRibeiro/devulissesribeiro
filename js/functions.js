ab313d624713a1debb0f8114bae4f9b592c412ec
ulisses gomes <ulissestark@gmail.com>
2022-04-08 21:27:31 -0300

    add jquery in projet

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ var visibilidade = true;
 
 
 
-function showProj1(){
+/*function showProj1(){
     document.getElementById("projeto1").style.display = "block";
 }
 function hideProj1(){
@@ -15,4 +15,31 @@ function showProj2(){
 }
 function hideProj2(){
     document.getElementById("projeto2").style.display = "none";
-}
+}*/
+$(document).ready(function(){
+    $("#loadProj1").click(function(){
+    $("#conteudo").load("/proj1.html");
+  });
+  });
+  $(document).ready(function(){
+    $("#loadProj1").click(function(){
+      $("#conteudo").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#loadProj2").click(function(){
+    $("#conteudo").load("/proj2.html");
+  });
+  });
+  $(document).ready(function(){
+    $("#loadProj2").click(function(){
+      $("#conteudo").show();
+    });
+  });
+
+$(document).ready(function(){
+    $("#fechar").click(function(){
+      $("#conteudo").hide();
+    });
+  });
